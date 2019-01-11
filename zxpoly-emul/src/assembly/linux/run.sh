19a872fb062a660393bfb816a872c69c6d0646cd
Igor Maznitsa <rrg4400@gmail.com>
2019-01-11 23:47:18 +0200

    improved distributing

--- zxpoly-emul/src/assembly/linux/run.sh
+++ zxpoly-emul/src/assembly/linux/run.sh
@@ -2,19 +2,14 @@
 
 ZXPOLY_HOME="$(dirname ${BASH_SOURCE[0]})"
 LOG_FILE=$ZXPOLY_HOME/console.log
+JAVA_HOME=$ZXPOLY_HOME/jre
 
 # uncomment the line below if graphics works slowly
 # JAVA_EXTRA_GFX_FLAGS="-Dsun.java2d.opengl=true"
 
 JAVA_FLAGS="-client -XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Dsun.java2d.dpiaware=true -Dswing.aatext=true -Dawt.useSystemAAFontSettings=on"
 
-if [ -z $JAVA_HOME ]; then
-    echo \$JAVA_HOME is undefined &>$LOG_FILE
-    JAVA_RUN=java
-else
-    echo Detected \$JAVA_HOME : $JAVA_HOME &>$LOG_FILE
-    JAVA_RUN=$JAVA_HOME/bin/java
-fi
+JAVA_RUN=$JAVA_HOME/bin/java
 
 if [ -f $ZXPOLY_HOME/.pid ];
 then
@@ -22,7 +17,7 @@ then
     if [ -f /proc/$SAVED_PID/exe ];
     then
         echo Emulator already started! if it is wrong, just delete the .pid file in the editor folder root!
-	exit 1
+        exit 1
     fi
 fi
 
