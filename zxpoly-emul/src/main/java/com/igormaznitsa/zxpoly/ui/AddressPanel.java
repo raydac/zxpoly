661b0317f111aa9c0375a6ccd51109f2758c80f5
Igor Maznitsa <rrg4400@gmail.com>
2019-01-19 21:42:52 +0200

    updated license header

--- zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/ui/AddressPanel.java
+++ zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/ui/AddressPanel.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2019 Igor Maznitsa
+ * Copyright (C) 2014-2019 Igor Maznitsa
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
