191b3ec43a75cef0afa821715eb5492a8d03d051
Igor Maznitsa <rrg4400@gmail.com>
2019-11-21 21:26:00 +0200

    started 2.0.1-SNAPSHOT

--- zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/Application.java
+++ zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/Application.java
@@ -31,7 +31,7 @@ import org.apache.commons.lang3.SystemUtils;
 public class Application {
 
   public static final String APP_TITLE = "ZX-Poly emulator";
-  public static final String APP_VERSION = "v 2.0.0";
+  public static final String APP_VERSION = "v 2.0.1";
 
   private static void setGnomeAppTitle() {
     try {
