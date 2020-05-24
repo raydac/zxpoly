b9fa5229cafd830d3143ba750d009a59753720e6
Igor Maznitsa <rrg4400@gmail.com>
2020-05-24 18:55:33 +0300

    updated script to make desktop icon in linux

--- zxpoly-emul/src/assembly/linux/make_desktop.sh
+++ zxpoly-emul/src/assembly/linux/make_desktop.sh
@@ -6,19 +6,18 @@ APP_HOME="$(realpath $(dirname ${BASH_SOURCE[0]}))"
 TARGET=$APP_HOME/zx-poly-emulator.desktop
 
 echo [Desktop Entry] > $TARGET
+echo Version=1.1 >> $TARGET
 echo Encoding=UTF-8 >> $TARGET
-echo Version=2.0.6 >> $TARGET
 echo Type=Application >> $TARGET
 echo Name=ZX-Poly emulator >> $TARGET
 echo GenericName=ZX-Poly emulator >> $TARGET
-echo Icon="$APP_HOME/logo.svg" >> $TARGET
-echo Exec="$APP_HOME/run.sh" %f >> $TARGET
+echo Icon=$APP_HOME/logo.svg >> $TARGET
+echo Exec=\"$APP_HOME/run.sh\" %f >> $TARGET
 echo Comment=Emulator of ZX-Poly platform >> $TARGET
-echo Categories=Game;Emulator; >> $TARGET
+echo "Categories=Game;Emulator;" >> $TARGET
+echo "Keywords=zx;spectrum;poly;emulator;game;" >> $TARGET
 echo Terminal=false >> $TARGET
 echo StartupWMClass=zx-poly-emulator >> $TARGET
-echo OnlyShowIn=Old; >> $TARGET
-echo StartupNotify=true >> $TARGET
 
 echo Desktop script has been generated: $TARGET
 
