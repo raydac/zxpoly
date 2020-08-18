f2b07715d04507957eb7fa8193104e4015411543
Igor Maznitsa <rrg4400@gmail.com>
2020-08-18 23:39:14 +0300

    started 2.0.8-SNAPSHOT

--- zxpoly-sprite-corrector/src/main/java/com/igormaznitsa/zxpspritecorrector/AboutDialog.java
+++ zxpoly-sprite-corrector/src/main/java/com/igormaznitsa/zxpspritecorrector/AboutDialog.java
@@ -31,7 +31,7 @@ public final class AboutDialog extends javax.swing.JDialog {
     super(parent, true);
     initComponents();
 
-    this.infoText.setText(this.infoText.getText().replace("${project.version}", "2.0.7"));
+    this.infoText.setText(this.infoText.getText().replace("${project.version}", "2.0.8"));
 
     this.setLocationRelativeTo(null);
   }
