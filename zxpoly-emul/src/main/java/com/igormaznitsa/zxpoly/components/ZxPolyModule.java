b3d6508e55ed813b413efcd8f3609b8fa31a81c6
Igor Maznitsa <rrg4400@gmail.com>
2020-08-30 15:22:57 +0300

    refactoring

--- zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/ZxPolyModule.java
+++ zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/ZxPolyModule.java
@@ -473,10 +473,12 @@ public final class ZxPolyModule implements IoDevice, Z80CPUBus, MemoryAccessProv
         this.lastM1Address = address;
         final int address_h = address >>> 8;
 
+        final boolean trdosEnabled = (value7FFD & PORTw_ZX128_ROM) != 0;
+
         if (this.trdosRomActive) {
-          this.trdosRomActive = address_h < 0x40;
+          this.trdosRomActive = trdosEnabled && address_h < 0x40;
         } else {
-          this.trdosRomActive = (value7FFD & PORTw_ZX128_ROM) != 0 && address_h == 0x3D;
+          this.trdosRomActive = trdosEnabled && address_h == 0x3D;
         }
       }
 
