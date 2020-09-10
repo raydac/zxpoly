646f45f92d845ca0436fe9f471149e8a3a89d10b
Igor Maznitsa <rrg4400@gmail.com>
2020-09-11 00:14:00 +0300

    refactoring

--- zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/snd/AySounder.java
+++ zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/snd/AySounder.java
@@ -1,28 +1,22 @@
 package com.igormaznitsa.zxpoly.components.snd;
 
-import static java.lang.Math.round;
-
-
-import java.util.Arrays;
-
 interface AySounder {
-  int[] AY_AMPLITUDE = Arrays.stream(new double[] {
-      0.0000d,
-      0.0137d,
-      0.0205d,
-      0.0291d,
-      0.0423d,
-      0.0618d,
-      0.0847d,
-      0.1369d,
-      0.1691d,
-      0.2647d,
-      0.3527d,
-      0.4499d,
-      0.5704d,
-      0.6873d,
-      0.8482d,
-      1.0000d
-  }).mapToInt(d -> (int) round(Beeper.AMPLITUDE_MAX * d)).toArray();
-
+  byte[] AY_AMPLITUDE = new byte[] {
+      0x00,
+      0x03,
+      0x05,
+      0x07,
+      0x0A,
+      0x0F,
+      0x15,
+      0x23,
+      0x29,
+      0x41,
+      0x5B,
+      0x72,
+      (byte) 0x90,
+      (byte) 0xB5,
+      (byte) 0xD8,
+      (byte) 0xFF
+  };
 }
