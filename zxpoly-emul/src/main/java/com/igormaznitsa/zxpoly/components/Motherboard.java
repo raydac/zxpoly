979aff3844208c3a18d744c53e0385cd1eb727cc
Igor Maznitsa <rrg4400@gmail.com>
2021-07-10 13:43:50 +0300

    changed t-syayes per int calc

--- zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/Motherboard.java
+++ zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/Motherboard.java
@@ -49,7 +49,7 @@ public final class Motherboard implements ZxPolyConstants {
   public static final int TSTATES_BEFORE_MAIN_AREA_OUTPUT = TSTATES_BEFORE_SCREEN_OUTPUT_START + SCREEN_VISIBLE_ROWS_BEFORE_MAIN_AREA * TSTATES_PER_ROW;
   public static final int TSTATES_MAIN_AREA_OUTPUT_END = TSTATES_BEFORE_MAIN_AREA_OUTPUT + 192 * TSTATES_PER_ROW;
   public static final int TSTATES_SCREEN_OUTPUT_END = TSTATES_MAIN_AREA_OUTPUT_END + SCREEN_VISIBLE_ROWS_AFTER_MAIN_AREA * TSTATES_PER_ROW;
-  public static final int TSTATES_PER_INT = TSTATES_SCREEN_OUTPUT_END;
+  public static final int TSTATES_PER_INT = CPU_FREQ / 50;
 
   public static final int TRIGGER_NONE = 0;
   public static final int TRIGGER_DIFF_MODULESTATES = 1;
