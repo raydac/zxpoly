a08443d0cb9a535f847d95238ac2d4d784196eff
Igor Maznitsa <rrg4400@gmail.com>
2021-08-07 00:48:29 +0300

    refactoring

--- zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/snd/MixerUtils.java
+++ zxpoly-emul/src/main/java/com/igormaznitsa/zxpoly/components/snd/MixerUtils.java
@@ -2,48 +2,51 @@ package com.igormaznitsa.zxpoly.components.snd;
 
 public abstract class MixerUtils {
 
+  private static final float FACTOR_MIDDLE = 0.5f;
+  private static final float FACTOR_MAIN = 0.6f;
+
   private static final float SCALE_FACTOR_6 = Short.MAX_VALUE / (255.0f * 6);
   private static final float SCALE_FACTOR_5 = Short.MAX_VALUE / (255.0f * 5);
   private static final float SCALE_FACTOR_4 = Short.MAX_VALUE / (255.0f * 4);
   private static final float SCALE_FACTOR_3 = Short.MAX_VALUE / (255.0f * 3);
 
-  protected static int scaleLeft6(final int left, final int center) {
-    final float mixed = left * 0.7f + center * 0.3f;
+  protected static int scaleLeft6(final int left, final int middle) {
+    final float mixed = left * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_6));
   }
 
-  protected static int scaleRight6(final int right, final int center) {
-    final float mixed = right * 0.7f + center * 0.3f;
+  protected static int scaleRight6(final int right, final int middle) {
+    final float mixed = right * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_6));
   }
 
-  protected static int scaleLeft5(final int left, final int center) {
-    final float mixed = left * 0.7f + center * 0.3f;
+  protected static int scaleLeft5(final int left, final int middle) {
+    final float mixed = left * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_5));
   }
 
-  protected static int scaleRight5(final int right, final int center) {
-    final float mixed = right * 0.7f + center * 0.3f;
+  protected static int scaleRight5(final int right, final int middle) {
+    final float mixed = right * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_5));
   }
 
-  protected static int scaleLeft4(final int left, final int center) {
-    final float mixed = left * 0.7f + center * 0.3f;
+  protected static int scaleLeft4(final int left, final int middle) {
+    final float mixed = left * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_4));
   }
 
-  protected static int scaleRight4(final int right, final int center) {
-    final float mixed = right * 0.7f + center * 0.3f;
+  protected static int scaleRight4(final int right, final int middle) {
+    final float mixed = right * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_4));
   }
 
-  protected static int scaleLeft3(final int left, final int center) {
-    final float mixed = left * 0.7f + center * 0.3f;
+  protected static int scaleLeft3(final int left, final int middle) {
+    final float mixed = left * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_3));
   }
 
-  protected static int scaleRight3(final int right, final int center) {
-    final float mixed = right * 0.7f + center * 0.3f;
+  protected static int scaleRight3(final int right, final int middle) {
+    final float mixed = right * FACTOR_MAIN + middle * FACTOR_MIDDLE;
     return Math.min(Short.MAX_VALUE, Math.round(mixed * SCALE_FACTOR_3));
   }
 
